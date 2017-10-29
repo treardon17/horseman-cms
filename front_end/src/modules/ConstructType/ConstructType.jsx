2a8386eec8e10ef8cec4f682310000120acea587
Tyler Reardon <tdr.reardon@gmail.com>
2017-10-29 13:19:28 -0700

    Ability to update types on the fly

--- front_end/src/modules/ConstructType/ConstructType.jsx
+++ front_end/src/modules/ConstructType/ConstructType.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 import Type from '../../resources/scripts/types/Type';
+import Select from 'react-select';
+import Button from 'material-ui/Button';
 
 // scss
 import './ConstructType.scss';
@@ -8,15 +11,87 @@ export default class ConstructType extends React.Component {
   constructor(props) {
     super(props);
 
-    const type = new Type();
-    type.add({ name: 'title', primary: Type.string });
-    type.add({ name: 'locations', primary: Type.list, secondary: Type.number });
-    console.log('type is:', type.getJSON());
+    // const type = new Type();
+    // type.add({ name: 'title', primary: Type.string });
+    // type.add({ name: 'locations', primary: Type.list, secondary: Type.number });
+    // console.log('type is:', type.getJSON());
+
+    this.state = {
+      type: this.props.type || new Type({ name: 'Module title' }),
+    };
+  }
+
+
+  getFields() {
+    // Get all of the slugs from the type.parts
+    const parts = this.state.type.parts;
+    const slugs = Object.keys(parts);
+    const fields = [];
+    console.log('getting fields');
+    for (let i = 0; i < slugs.length; i++) {
+      const slug = slugs[i];
+      const part = parts[slug];
+      fields.push(
+        <div className="field" key={`field-${i}`}>
+          <input
+            type="text"
+            value={part.name}
+            onChange={(e) => { this.handleChangePart(e, 'name', slug); }}
+          />
+          <Select />
+        </div>
+      );
+    }
+    return fields;
+  }
+
+  handleChangePart(e, partID, slug) {
+    const type = this.state.type;
+    const editObject = {};
+    editObject[partID] = e.target.value;
+    editObject.slug = slug;
+    type.edit(editObject);
+    console.log('type is:', type.parts);
+    this.setState({ type });
+  }
+
+  addField() {
+    const type = this.state.type;
+    type.add();
+    this.setState({ type });
+    console.log(this.state.type);
+  }
+
+  handleNameChange(e) {
+    const type = this.state.type;
+    type.name = e.target.value;
+    this.setState({ type });
+  }
+
+  fieldTemplate() {
+    // Gotta figure out a good way to add a new field
+    // and listen for the changes on that new field
+    // to dynamically update this.state.type, but also be
+    // able to edit previous fields
   }
 
   render() {
     return (
-      <div className="construct-type" />
+      <div className="construct-type">
+        <input
+          type="text"
+          value={this.state.type.name}
+          onChange={this.handleNameChange.bind(this)}
+        />
+        <div className="constructor-container">
+          {this.getFields()}
+        </div>
+        <Button onClick={this.addField.bind(this)}>Add</Button>
+      </div>
     );
   }
 }
+
+ConstructType.propTypes = {
+  type: Proptypes.object,
+};
--- front_end/src/resources/scripts/types/Type.js
+++ front_end/src/resources/scripts/types/Type.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 
 export default class Type {
   /**
@@ -18,6 +19,37 @@ export default class Type {
   }
   /* eslint-enable */
 
+
+  /**
+   * findUniqueSlug - Finds a unique hash in the `parts` Object
+   * so we don't override an existing object
+   *
+   * @param  {String} { slug } description   The string that will be tested
+   * @return {String}          description   A unique hash for the `parts` object
+   */
+  findUniqueSlug({ slug }) {
+    const key = _.camelCase(slug);
+    // If a slug already exists
+    if (this.parts[key] != null) {
+      // If there's already a number at the end
+      const pattern = /([0-9]+)/i;
+      const matches = key.match(pattern);
+
+      let slugNum = 1;
+      let newSlug = key;
+      if (matches) {
+        // Replace the current number and increment it
+        slugNum = parseInt(matches[1]) + 1;
+        newSlug = newSlug.replace(matches[0], '');
+      }
+
+      // Return the new number and check if it's available
+      return this.findUniqueSlug({ slug: `${newSlug}${slugNum}` });
+    } else {
+      return key;
+    }
+  }
+
   /**
    * add - Add properties to the type object
    *
@@ -27,7 +59,8 @@ export default class Type {
    * @param  {type}   description   description   A description of the property being added
    * @return {void}
    */
-  add({ name = 'default', primary = Type.empty, secondary = null, description } = {}) {
+  add({ name = 'New field', primary = Type.empty, secondary = null, description = 'Field description' } = {}) {
+    const slug = this.findUniqueSlug({ slug: name });
     // Seconary types should only be specified for object types
     // or for list types that require additional information
     let skip = false;
@@ -35,11 +68,35 @@ export default class Type {
       skip = true;
       console.warn(`Seconary type not permitted when the Primary type is not of type ${Type.object} or ${Type.list}`);
     }
-    this.parts[name] = {
+    this.parts[slug] = {
+      name,
       primary: primary || Type.empty,
       seconary: !skip ? (secondary || Type.empty) : Type.empty,
       description: description || '',
     };
+  }
+
+  edit(arg = { }) {
+    const { slug = '', name, primary, secondary, description } = arg || {};
+    // If we changed the name, we change the slug
+    const newSlug = this.findUniqueSlug({ slug: name });
+    const part = this.parts[slug];
+    if (newSlug !== slug) {
+      this.parts[newSlug] = part;
+      delete this.parts[slug];
+    }
+    // Set the new values if they exist
+    if (part) {
+      const keys = Object.keys(arg);
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        part[key] = arg[key];
+      }
+      // Change the part location to be at the new slug
+      this.parts[newSlug] = part;
+    } else {
+      console.error('Cannot edit null object');
+    }
   }
 
   remove({ name }) {
