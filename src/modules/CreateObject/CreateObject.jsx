579df913cf011539b169a12d040c4c2c7bec3c77
Tyler Reardon <tdr.reardon@gmail.com>
2017-12-17 23:18:05 -0800

    Add pseudocode flow for CreateObject

--- src/modules/CreateObject/CreateObject.jsx
+++ src/modules/CreateObject/CreateObject.jsx
@@ -7,6 +7,27 @@ import TypeState from '../../state/TypeState';
 // scss
 import './ConstructObject.scss';
 
+/**
+ * Flow ---
+ * 1. user select which type they want to create an instance of
+ *
+ * 2. grab type definition and loop through children
+ *
+ * 3. if child typePrimary is anything other than a list, object, or module,
+ *    grab the appropriate input box and display it
+ *
+ * 4. if child typePrimary is an object, or module, make another CreateObject
+ *    instance and display those input fields tabbed over a little bit
+ *
+ * 5. if child typePrimary is a list, show an 'add' button that either displays
+ *    a simple input field like those mentioned above but defined in the typeSecondary,
+ *    or creates another CreateObject instance of whatever type is specified
+ *    in the typeSecondary
+ *
+ * ** Note **
+ * This is a recursively defined class, so be wary of infinite recursion.
+ */
+
 export default class CreateObject extends Creator {
   constructor(props) {
     super(props);
