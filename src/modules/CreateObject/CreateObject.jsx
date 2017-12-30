a54d013e78d918dcd23473eb764a9527a87a1b9f
Tyler Reardon <tdr.reardon@gmail.com>
2017-12-29 19:48:51 -0800

    Fix input editing not working

--- src/modules/CreateObject/CreateObject.jsx
+++ src/modules/CreateObject/CreateObject.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 import Creator from '../Creator/Creator';
+import _ from 'lodash';
 
 import DataState from '../../state/DataState';
 import TypeState from '../../state/TypeState';
@@ -37,37 +38,84 @@ export default class CreateObject extends Creator {
 
     this.state = {
       ...super.state,
+      current: this.props.childObject,
     };
   }
 
-  getFields() {
-    const fields = [];
-    const childObject = this.props.childObject;
-    if (childObject) {
-      const { _typeID, _id } = childObject;
-      const type = TypeState.userMadeTypes.get(_typeID);
-      const keys = Object.keys(type.children);
-      for (let i = 0; i < keys.length; i++) {
-        const key = keys[i];
-        const child = type.get(key);
-        const childVal = childObject[child.slug];
+  // getFields() {
+  //   const fields = [];
+  //   const childObject = this.props.childObject;
+  //   if (childObject) {
+  //     const { _typeID, _id } = childObject;
+  //     const type = TypeState.userMadeTypes.get(_typeID);
+  //     const keys = Object.keys(type.children);
+  //     for (let i = 0; i < keys.length; i++) {
+  //       const key = keys[i];
+  //       const child = type.get(key);
+  //       const childVal = childObject[child.slug];
+  //
+  //       if (child.typePrimary === ObjectType.types.module) {
+  //         // We need to be able to recursively call subtypes here
+  //         const field = (<CreateObject key={`construct-object-${_id}-${i}`} childObject={childVal} keyIndex={i} />);
+  //         fields.push(field);
+  //       } else if (child.typePrimary === ObjectType.types.string) {
+  //         fields.push(<input key={`${ObjectType.types.string}-${_id}`} value={childVal} data-type={ObjectType.types.string} />);
+  //       } else if (child.typePrimary === ObjectType.types.number) {
+  //         fields.push(<input key={`${ObjectType.types.number}-${_id}`} value={childVal} data-type={ObjectType.types.number} />);
+  //       }
+  //     }
+  //   }
+  //   return fields;
+  // }
 
-        if (child.typePrimary === ObjectType.types.module) {
-          // We need to be able to recursively call subtypes here
-          const field = (<CreateObject key={`construct-object-${_id}-${i}`} childObject={childVal} keyIndex={i} />);
-          fields.push(field);
-        } else if (child.typePrimary === ObjectType.types.string) {
-          fields.push(<input key={`${ObjectType.types.string}-${_id}`} value={childVal} data-type={ObjectType.types.string} />);
-        } else if (child.typePrimary === ObjectType.types.number) {
-          fields.push(<input key={`${ObjectType.types.number}-${_id}`} value={childVal} data-type={ObjectType.types.number} />);
-        }
+  handleFieldChange(event, nestedIDs) {
+    this.saveStateIfNeeded();
+    const newVal = this.nestedObject({ object: this.state.current, idArray: nestedIDs, newVal: event.target.value });
+    this.setState({ current: newVal });
+  }
+
+  nestedObject({ object, idArray, newVal = null }) {
+    let value = _.cloneDeep(object);
+    for (let i = 0; i < idArray.length; i++) {
+      const id = idArray[i];
+      if ((newVal || newVal === '') && i === idArray.length - 1) {
+        value[id] = newVal;
+      } else {
+        // Get the next nested value
+        value = value[id];
+      }
+    }
+    return value;
+  }
+
+  getObjectFields(childObject, nestedWithin = []) {
+    const { _typeID, _id } = childObject;
+    const type = TypeState.userMadeTypes.get(_typeID);
+    const fields = [];
+    const keys = Object.keys(type.children);
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const child = type.get(key);
+      const childVal = childObject[child.slug];
+      const guid = `${ObjectType.types.string}-${_id}-${i}`;
+      const nestedIDs = [];
+      nestedIDs.concat(nestedWithin);
+      nestedIDs.push(child.slug);
+
+      if (child.typePrimary === ObjectType.types.module) {
+        // We need to be able to recursively call subtypes here
+        fields.concat(this.getObjectFields(childVal, nestedIDs));
+      } else if (child.typePrimary === ObjectType.types.string) {
+        fields.push(<input key={guid} value={this.nestedObject({ object: this.state.current, idArray: nestedIDs })} onChange={(e) => { this.handleFieldChange(e, nestedIDs); }} data-type={ObjectType.types.string} />);
+      } else if (child.typePrimary === ObjectType.types.number) {
+        fields.push(<input key={guid} value={this.nestedObject({ object: this.state.current, idArray: nestedIDs })} onChange={(e) => { this.handleFieldChange(e, nestedIDs); }} data-type={ObjectType.types.number} />);
       }
     }
     return fields;
   }
 
   render() {
-    const fields = this.getFields();
+    const fields = this.getObjectFields(this.props.childObject);
     return (
       <div className="create-object">
         {fields}
