7f6e307e57da79fdeec7657635e857ae4d64d29b
Tyler Reardon <tdr.reardon@gmail.com>
2018-01-20 22:03:36 -0800

    Remove unused files

--- back_end/src/schema/index.js
+++
@@ -1,7 +0,0 @@
-const { GQC }  =  require('graphql-compose');
-
-const ModuleSettingsSchema = require('./module_settings.js');
-const ModuleSchema = require('./module.js');
-
-const graphqlSchema = GQC.buildSchema();
-module.exports = graphqlSchema;
